cd9eb75d34d27cb02d74e2a1881d69ba985d5866
BioHazard <3482991796@qq.com>
2025-09-06 15:42:38 +0800

    2025-09-06

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from typing import NamedTuple, Self
+from collections.abc import Iterable, Sequence
 from fractions import Fraction
+from typing import Literal, NamedTuple, Self
 
 
 class 订单详情(NamedTuple):
@@ -14,9 +15,20 @@ class 贸易站数据:
         self.订单列表: list[订单详情] = 订单列表
 
     @property
+    def 需要秒基础工时期望(self) -> Fraction:
+        return sum(订单.概率 * 订单.需要秒基础工时 for 订单 in self.订单列表)  # type: ignore
+
+    @property
+    def 消耗赤金期望(self) -> Fraction:
+        return sum(订单.概率 * 订单.消耗赤金 for 订单 in self.订单列表)  # type: ignore
+
+    @property
+    def 获得龙门币期望(self) -> Fraction:
+        return sum(订单.概率 * 订单.获得龙门币 for 订单 in self.订单列表)  # type: ignore
+
+    @property
     def 每秒基础工时获得龙门币(self) -> Fraction:
-        return (sum(订单.概率 * 订单.获得龙门币 for 订单 in self.订单列表)
-                / sum(订单.概率 * 订单.需要秒基础工时 for 订单 in self.订单列表))  # type: ignore
+        return self.获得龙门币期望 / self.需要秒基础工时期望
 
     @property
     def 每天基础工时获得龙门币(self) -> Fraction:
@@ -24,7 +36,7 @@ class 贸易站数据:
 
     @property
     def 每秒基础工时消耗赤金(self) -> Fraction:
-        return sum(订单.概率 * 订单.消耗赤金 for 订单 in self.订单列表) / sum(订单.概率 * 订单.需要秒基础工时 for 订单 in self.订单列表)  # type: ignore
+        return self.消耗赤金期望 / self.需要秒基础工时期望
 
     @property
     def 每天基础工时消耗赤金(self) -> Fraction:
@@ -32,7 +44,7 @@ class 贸易站数据:
 
     @property
     def 平均每赤金获得龙门币(self) -> Fraction:
-        return self.每秒基础工时获得龙门币 / self.每秒基础工时消耗赤金
+        return self.获得龙门币期望 / self.消耗赤金期望
 
     @property
     def 每秒基础工时节省赤金(self) -> Fraction:
@@ -58,6 +70,10 @@ class 贸易站数据:
     def 钱书基础工时成本比(self) -> Fraction:
         return self.生产1龙门币需要的秒基础工时 / Fraction(54, 5)
 
+    @property
+    def 书钱基础工时成本比(self) -> Fraction:
+        return 1 / self.钱书基础工时成本比
+
     @classmethod
     def new(cls, 贸易站等级: int, 但书: int | None, 龙舌兰: int | None, 裁缝: str | None) -> Self:
         需要秒基础工时列表: list[int] = [8640, 12600, 16560]
@@ -68,9 +84,9 @@ class 贸易站数据:
         elif 贸易站等级 == 3:
             if 裁缝 is None:
                 订单概率列表 = [Fraction(30, 100), Fraction(50, 100), Fraction(20, 100)]
-            elif 裁缝 in ('α', 'alpha'):
+            elif 裁缝 in ("α", "alpha"):
                 订单概率列表 = [Fraction(15, 100), Fraction(30, 100), Fraction(55, 100)]
-            elif 裁缝 in ('β', 'beta'):
+            elif 裁缝 in ("β", "beta"):
                 订单概率列表 = [Fraction(5, 100), Fraction(10, 100), Fraction(85, 100)]
             else:
                 raise ValueError
@@ -106,55 +122,129 @@ class 贸易站数据:
         return cls(订单列表)
 
 
+type Style = Literal["p / q = v", "p / q", "v"]
+
+
+def output_str(value: Fraction, style: Style = "p / q = v") -> str:
+    if value.denominator == 1:
+        return f"{value.numerator}"
+    else:
+        match style:
+            case "p / q = v":
+                return f"{value.numerator} / {value.denominator} = {float(value)}"
+            case "p / q":
+                return f"{value.numerator} / {value.denominator}"
+            case "v":
+                return f"{float(value)}"
+
+
+def to_markdown_table(data: Sequence[Iterable[str]], include_header: bool = True) -> str:
+    lines: list[str] = []
+    if include_header:
+        lines.append("| " + " | ".join(data[0]) + " |")
+        lines.append("| " + " | ".join("---" for _ in data[0]) + " |")
+        data = data[1:]
+    for row in data:
+        lines.append("| " + " | ".join(row) + " |")
+    return "\n".join(lines)
+
+
 贸易站汇总: dict[str, 贸易站数据] = {
-    '1 级龙门币贸易站': 贸易站数据.new(贸易站等级=1, 但书=None, 龙舌兰=None, 裁缝=None),
-    '1 级龙门币贸易站，进驻但书 II': 贸易站数据.new(贸易站等级=1, 但书=2, 龙舌兰=None, 裁缝=None),
-    '2 级龙门币贸易站': 贸易站数据.new(贸易站等级=2, 但书=None, 龙舌兰=None, 裁缝=None),
-    '2 级龙门币贸易站，进驻但书 II': 贸易站数据.new(贸易站等级=2, 但书=2, 龙舌兰=None, 裁缝=None),
-    '3 级龙门币贸易站': 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=None, 裁缝=None),
-    '3 级龙门币贸易站，进驻裁缝·α': 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=None, 裁缝='α'),
-    '3 级龙门币贸易站，进驻裁缝·β': 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=None, 裁缝='β'),
-    '3 级龙门币贸易站，进驻但书 II': 贸易站数据.new(贸易站等级=3, 但书=2, 龙舌兰=None, 裁缝=None),
-    '3 级龙门币贸易站，进驻龙舌兰 II': 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=2, 裁缝=None),
-    '3 级龙门币贸易站，进驻龙舌兰 II、裁缝·α': 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=2, 裁缝='α'),
-    '3 级龙门币贸易站，进驻龙舌兰 II、裁缝·β': 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=2, 裁缝='β'),
-    '3 级龙门币贸易站，进驻但书 II、龙舌兰 II': 贸易站数据.new(贸易站等级=3, 但书=2, 龙舌兰=2, 裁缝=None),
-    '3 级龙门币贸易站，进驻但书 II、龙舌兰 II、裁缝·α': 贸易站数据.new(
-        贸易站等级=3, 但书=2, 龙舌兰=2, 裁缝='α'),
-    '3 级龙门币贸易站，进驻但书 II、龙舌兰 II、裁缝·β': 贸易站数据.new(
-        贸易站等级=3, 但书=2, 龙舌兰=2, 裁缝='β'),
-    '一个 3 级龙门币贸易站进驻但书 II、另一个进驻龙舌兰 II + 裁缝·β，两个贸易站的干员效率相等': 贸易站数据([
-        订单详情(Fraction(30, 200), 8640, 4, 2000),
-        订单详情(Fraction(50, 200), 12600, 5, 2500),
-        订单详情(Fraction(20, 200), 16560, 4, 2000),
-        订单详情(Fraction(5, 200), 8640, 2, 1000),
-        订单详情(Fraction(10, 200), 12600, 3, 1500),
-        订单详情(Fraction(85, 200), 16560, 4, 2500),
-    ])
+    "1 级龙门币贸易站": 贸易站数据.new(贸易站等级=1, 但书=None, 龙舌兰=None, 裁缝=None),
+    "1 级龙门币贸易站，进驻但书 II": 贸易站数据.new(贸易站等级=1, 但书=2, 龙舌兰=None, 裁缝=None),
+    "2 级龙门币贸易站": 贸易站数据.new(贸易站等级=2, 但书=None, 龙舌兰=None, 裁缝=None),
+    "2 级龙门币贸易站，进驻但书 II": 贸易站数据.new(贸易站等级=2, 但书=2, 龙舌兰=None, 裁缝=None),
+    "3 级龙门币贸易站": 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=None, 裁缝=None),
+    "3 级龙门币贸易站，进驻裁缝·α": 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=None, 裁缝="α"),
+    "3 级龙门币贸易站，进驻裁缝·β": 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=None, 裁缝="β"),
+    "3 级龙门币贸易站，进驻但书 II": 贸易站数据.new(贸易站等级=3, 但书=2, 龙舌兰=None, 裁缝=None),
+    "3 级龙门币贸易站，进驻龙舌兰 II": 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=2, 裁缝=None),
+    "3 级龙门币贸易站，进驻龙舌兰 II、裁缝·α": 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=2, 裁缝="α"),
+    "3 级龙门币贸易站，进驻龙舌兰 II、裁缝·β": 贸易站数据.new(贸易站等级=3, 但书=None, 龙舌兰=2, 裁缝="β"),
+    "3 级龙门币贸易站，进驻但书 II、龙舌兰 II": 贸易站数据.new(贸易站等级=3, 但书=2, 龙舌兰=2, 裁缝=None),
+    "3 级龙门币贸易站，进驻但书 II、龙舌兰 II、裁缝·α": 贸易站数据.new(贸易站等级=3, 但书=2, 龙舌兰=2, 裁缝="α"),
+    "3 级龙门币贸易站，进驻但书 II、龙舌兰 II、裁缝·β": 贸易站数据.new(贸易站等级=3, 但书=2, 龙舌兰=2, 裁缝="β"),
 }
 
 
-def output_str(value: Fraction) -> str:
-    if value.denominator == 1:
-        return f'{value.numerator}'
-    else:
-        return f'{value.numerator} / {value.denominator} = {float(value)}'
+columns = [
+    "贸易站名称",
+    "每秒基础工时获得龙门币",
+    "每天基础工时获得龙门币",
+    "每秒基础工时消耗赤金",
+    "每天基础工时消耗赤金",
+    "平均每赤金获得龙门币",
+    "每秒基础工时节省赤金",
+    "每天基础工时节省赤金",
+    "每秒基础工时印钱",
+    "每天基础工时印钱",
+    "生产 1 龙门币需要的秒基础工时",
+    "钱书基础工时成本比",
+    "书钱基础工时成本比",
+]
 
 
 lines: list[str] = []
 for 贸易站名称, 贸易站 in 贸易站汇总.items():
-    lines.append(f'## {贸易站名称}')
-    lines.append(f'每秒基础工时获得龙门币 = {output_str(贸易站.每秒基础工时获得龙门币)}')
-    lines.append(f'每天基础工时获得龙门币 = {output_str(贸易站.每天基础工时获得龙门币)}')
-    lines.append(f'每秒基础工时消耗赤金 = {output_str(贸易站.每秒基础工时消耗赤金)}')
-    lines.append(f'每天基础工时消耗赤金 = {output_str(贸易站.每天基础工时消耗赤金)}')
-    lines.append(f'平均每赤金获得龙门币 = {output_str(贸易站.平均每赤金获得龙门币)}')
-    lines.append(f'每秒基础工时节省赤金 = {output_str(贸易站.每秒基础工时节省赤金)}')
-    lines.append(f'每天基础工时节省赤金 = {output_str(贸易站.每天基础工时节省赤金)}')
-    lines.append(f'每秒基础工时印钱 = {output_str(贸易站.每秒基础工时印钱)}')
-    lines.append(f'每天基础工时印钱 = {output_str(贸易站.每天基础工时印钱)}')
-    lines.append(f'生产 1 龙门币需要的秒基础工时 = {output_str(贸易站.生产1龙门币需要的秒基础工时)}')
-    lines.append(f'钱书基础工时成本比 = {output_str(贸易站.钱书基础工时成本比)}')
+    lines.append(f"## {贸易站名称}")
+    lines.append(f"")
 
-with open('output.md', 'w', encoding='utf-8') as fp:
-    fp.write('\n'.join(lines))
+    data: list[list[str]] = [["概率", "需要秒基础工时", "消耗赤金数", "获得龙门币"]]
+    for 订单 in 贸易站.订单列表:
+        data.append([
+            f"{output_str(订单.概率 * 100, "v")}%",
+            f"{订单.需要秒基础工时}",
+            f"{订单.消耗赤金}",
+            f"{订单.获得龙门币}",
+        ])
+    data.append([
+        "综合",
+        output_str(贸易站.需要秒基础工时期望, "v"),
+        output_str(贸易站.消耗赤金期望, "v"),
+        output_str(贸易站.获得龙门币期望, "v"),
+    ])
+
+    lines.append(to_markdown_table(data))
+    lines.append(f"")
+
+    lines.append(f"- 每秒基础工时获得龙门币 = {output_str(贸易站.每秒基础工时获得龙门币)}")
+    lines.append(f"- 每天基础工时获得龙门币 = {output_str(贸易站.每天基础工时获得龙门币)}")
+    lines.append(f"- 每秒基础工时消耗赤金 = {output_str(贸易站.每秒基础工时消耗赤金)}")
+    lines.append(f"- 每天基础工时消耗赤金 = {output_str(贸易站.每天基础工时消耗赤金)}")
+    lines.append(f"- 平均每赤金获得龙门币 = {output_str(贸易站.平均每赤金获得龙门币)}")
+    lines.append(f"- 每秒基础工时节省赤金 = {output_str(贸易站.每秒基础工时节省赤金)}")
+    lines.append(f"- 每天基础工时节省赤金 = {output_str(贸易站.每天基础工时节省赤金)}")
+    lines.append(f"- 每秒基础工时印钱 = {output_str(贸易站.每秒基础工时印钱)}")
+    lines.append(f"- 每天基础工时印钱 = {output_str(贸易站.每天基础工时印钱)}")
+    lines.append(f"- 生产 1 龙门币需要的秒基础工时 = {output_str(贸易站.生产1龙门币需要的秒基础工时)}")
+    lines.append(f"- 钱书基础工时成本比 = {output_str(贸易站.钱书基础工时成本比)}")
+    lines.append(f"- 书钱基础工时成本比 = {output_str(贸易站.书钱基础工时成本比)}")
+    lines.append("")
+
+
+for style in ("p / q", "v"):
+    data: list[list[str]] = [columns]
+
+    for 贸易站名称, 贸易站 in 贸易站汇总.items():
+        line = [
+            贸易站名称,
+            output_str(贸易站.每秒基础工时获得龙门币, style),
+            output_str(贸易站.每天基础工时获得龙门币, style),
+            output_str(贸易站.每秒基础工时消耗赤金, style),
+            output_str(贸易站.每天基础工时消耗赤金, style),
+            output_str(贸易站.平均每赤金获得龙门币, style),
+            output_str(贸易站.每秒基础工时节省赤金, style),
+            output_str(贸易站.每天基础工时节省赤金, style),
+            output_str(贸易站.每秒基础工时印钱, style),
+            output_str(贸易站.每天基础工时印钱, style),
+            output_str(贸易站.生产1龙门币需要的秒基础工时, style),
+            output_str(贸易站.钱书基础工时成本比, style),
+            output_str(贸易站.书钱基础工时成本比, style),
+        ]
+        data.append(line)
+
+    lines.append(to_markdown_table(data))
+    lines.append("")
+
+with open("output.md", "w", encoding="utf-8") as fp:
+    fp.write("\n".join(lines))
